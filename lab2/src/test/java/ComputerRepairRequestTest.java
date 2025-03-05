30eeeb8ec1b85f04d8993e289a98886a8035780f
DTwain <obrejadavid8@gmail.com>
2025-03-05 17:15:27 +0200

    lab 2 done

---
+++ lab2/src/test/java/ComputerRepairRequestTest.java
@@ -0,0 +1,24 @@
+import ro.mpp2024.model.ComputerRepairRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ComputerRepairRequestTest {
+    @Test
+    @DisplayName("First text")
+    public void testFirstText() {
+        ComputerRepairRequest computerRepairRequest = new ComputerRepairRequest();
+        assertEquals("", computerRepairRequest.getOwnerName());
+        assertEquals("", computerRepairRequest.getOwnerAddress());
+    }
+
+    @Test
+    @DisplayName("Second text")
+    public void testExemplu() {
+        assertEquals(2, 2, "Numele ar trebui sa fie egale");
+    }
+
+
+}
